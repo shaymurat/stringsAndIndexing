62ec7ff7c86090287c1c392c6c934bf52c404c56
shaymurat <shaymuratoff@ya.ru>
2024-07-19 03:03:01 +0500

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,10 @@
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+
+print(example[5:])
+# общий вариант для любой строки
+# print(example[len(example) // 2 + (len(example) - len(example) // 2 + 1) % 2:])
+
+print(example[::-1])
+print(example[1::2])
